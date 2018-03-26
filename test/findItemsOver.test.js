5b5a8a67614877ac87b0a4ae12440da915ea5db3
Thembisile <shaundamon6@icloud.com>
2018-03-26 16:34:11 +0200

    changes to tests

--- test/findItemsOver.test.js
+++ test/findItemsOver.test.js
@@ -10,18 +10,18 @@ describe('findItemsOver', function(){
 ];
     assert.deepEqual(findItemsOver(itemList, threshold), [{name:'pears', qty: 37}, {name: 'bananas', qty: 27},]);
   });
-  it('should return products that have quantity higher than the threshold.', function(){
+  it('should return items over threshold, if none then it should return nothing.', function(){
     //Add two variables for threshold and itemList
     var threshold = 13;
     var itemList = [
     {name : 'apples', qty : 10},
-    {name : 'pears', qty : 37},
-    {name : 'bananas', qty : 27},
+    {name : 'pears', qty : 7},
+    {name : 'bananas', qty : 12},
     {name : 'apples', qty : 3},
 ];
-    assert.notEqual(findItemsOver(itemList, threshold), [{name:'apples', qty: 37},{name: 'apples', qty: 10},]);
+    assert.notEqual(findItemsOver(itemList, threshold), ' ');
   });
-  it('should return no quantity if there is no items in the list', function(){
+  it('should return an empty list if there is no items in the list', function(){
     var items = [''];
     assert.equal(findItemsOver(items), '')
   })
--- test/findItemsOver20.test.js
+++ test/findItemsOver20.test.js
@@ -9,18 +9,27 @@ describe('findItemsOver20', function(){
     assert.deepEqual(findItemsOver20(itemList), [{name : 'pears', qty : 37},
     {name : 'bananas', qty : 27},]);
   });
-  it('should return all products that are over 20', function(){
+  it('should return all products, if they are all over 20', function(){
   var itemList = [
-      {name : 'apples', qty : 10},
+      {name : 'apples', qty : 30},
       {name : 'pears', qty : 37},
       {name : 'bananas', qty : 27},
-      {name : 'apples', qty : 3},
+      {name : 'apples', qty : 23},
   ];
-  assert.notEqual(findItemsOver20(itemList), [{name : 'apples', qty : 10},
-  {name : 'apples', qty : 3},]);
+  assert.notEqual(findItemsOver20(itemList), [{name : 'apples', qty : 30},
+  {name : 'apples', qty : 23}, {name : 'bananas', qty : 27}, {name : 'pears', qty : 37},]);
   });
-  it('should return nothing if there is no items in List', function(){
+  it('should return an nothing if there is no products in List', function(){
     var list = [''];
     assert.equal(findItemsOver20(list), '')
+  })
+  it('should return an empty list if all products are less than 20', function(){
+  var itemList = [
+      {name : 'apples', qty : 10},
+      {name : 'pears', qty : 17},
+      {name : 'bananas', qty : 7},
+      {name : 'apples', qty : 3},
+    ];
+    assert.notEqual(findItemsOver20(itemList), [])
   })
 });
